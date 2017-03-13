c807ce676c227fd12aad3652273dec6f56ecdf7e
Stephan Traub <sbidy@hotmail.com>
2017-03-13 17:29:43 +0100

    Revert "Revert "Init. upload""
    This reverts commit 937bbd3ee611a46cfcd6906489596a6dcb54ffa9.

---
+++ privacyIDEAADFSProvider/AdapterPresentationForm.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.IdentityServer.Web.Authentication.External;
+
+namespace privacyIDEAADFSProvider
+{
+    class AdapterPresentationForm : IAdapterPresentationForm
+    {
+        public string errormessage = "";
+        public string wellcomemassage = "";
+        private bool error = false;
+
+        public AdapterPresentationForm(bool error, string errormessage)
+        {
+            this.error = error;
+            this.errormessage = errormessage;
+        }
+        public AdapterPresentationForm(string wellcomemassage)
+        {
+            this.wellcomemassage = wellcomemassage;
+        }
+
+        /// Returns the HTML Form fragment that contains the adapter user interface. This data will be included in the web page that is presented
+        /// to the cient.
+        public string GetFormHtml(int lcid)
+        {
+            string htmlTemplate = Resources.AuthPage; // return normal page
+            if (error)
+            {
+                htmlTemplate = htmlTemplate.Replace("#ERROR#", errormessage);
+                htmlTemplate = htmlTemplate.Replace("#MESSAGE#", wellcomemassage);
+            }
+            else
+            {
+                htmlTemplate = htmlTemplate.Replace("#MESSAGE#", wellcomemassage);
+                htmlTemplate = htmlTemplate.Replace("#ERROR#", "");
+            }
+            return htmlTemplate;
+        }
+
+        /// Return any external resources, ie references to libraries etc., that should be included in 
+        /// the HEAD section of the presentation form html. 
+        public string GetFormPreRenderHtml(int lcid)
+        {
+            return null;
+        }
+
+        //returns the title string for the web page which presents the HTML form content to the end user
+        public string GetPageTitle(int lcid)
+        {
+            return "privacyIDEA OTP Adapter";
+        }
+
+    }
+}
---
+++ privacyIDEAADFSProvider/Install/Install-ADFSProvider.ps1
@@ -0,0 +1,19 @@
+﻿# Install the provider
+
+Set-location "C:\Windows\System32\privacyIDEAprovider"
+[System.Reflection.Assembly]::Load("System.EnterpriseServices, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")
+$publish = New-Object System.EnterpriseServices.Internal.Publish
+$publish.GacInstall("C:\Windows\System32\privacyIDEAprovider\privacyIDEAADFSProvider.dll")
+
+$typeName = "privacyIDEAADFSProvider.Adapter, privacyIDEAADFSProvider, Version=1.0.0.0, Culture=neutral, PublicKeyToken=b6483f285cb7b6eb"
+Register-AdfsAuthenticationProvider -TypeName $typeName -Name "privacyIDEAADFSProvider" -ConfigurationFilePath "C:\Windows\System32\privacyIDEAprovider\config.xml" -Verbose
+
+# Remove the provider
+
+Unregister-AdfsAuthenticationProvider -Name "privacyIDEAADFSProvider"
+Set-location "C:\Windows\System32\privacyIDEAprovider"
+[System.Reflection.Assembly]::Load("System.EnterpriseServices, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")
+$publish = New-Object System.EnterpriseServices.Internal.Publish
+$publish.GacRemove("C:\Windows\System32\privacyIDEAprovider\privacyIDEAADFSProvider.dll")
+
+Restart-Service adfssrv
