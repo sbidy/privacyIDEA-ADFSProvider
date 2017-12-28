357a331c759b890c54028d0e2cf61a31fbaa075f
Stephan Traub <stetrau@aol.com>
2017-12-28 14:49:24 +0100

    Update Remove-ADFSProvider.ps1

--- privacyIDEAADFSProvider/Install/Remove-ADFSProvider.ps1
+++ privacyIDEAADFSProvider/Install/Remove-ADFSProvider.ps1
@@ -1,9 +1,9 @@
 ﻿# Remove the provider
 
 Unregister-AdfsAuthenticationProvider -Name "privacyIDEA-ADFSProvider"
-Set-location "C:\Program and Files\privacyIDEAProvider\"
+Set-location "C:\Program Files\privacyIDEAProvider\"
 [System.Reflection.Assembly]::Load("System.EnterpriseServices, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")
 $publish = New-Object System.EnterpriseServices.Internal.Publish
-$publish.GacRemove("C:\Program and Files\privacyIDEAProvider\privacyIDEA-ADFSProvider.dll")
+$publish.GacRemove("C:\Program Files\privacyIDEAProvider\privacyIDEA-ADFSProvider.dll")
 
 Restart-Service adfssrv
